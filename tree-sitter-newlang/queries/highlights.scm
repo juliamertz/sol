c4ce89944469b8774675d388fbe918b726f2ccb1
Julia Mertz <info@juliamertz.dev>
2025-06-09 00:59:45 +0200

    fix hightlights

--- tree-sitter-newlang/queries/highlights.scm
+++ tree-sitter-newlang/queries/highlights.scm
@@ -8,27 +8,6 @@
   "use"
 ] @keyword
 
-; types
-(type) @type
-
-; function names
-(function_def
-  name: (identifier) @function)
-
-(call
-  function: (identifier) @function.call)
-
-; parameters
-(param
-  (identifier) @variable.parameter)
-
-; variables
-(identifier) @variable
-
-; literals
-(number) @number
-(string) @string
-
 ; operators
 [
   "<"
@@ -49,3 +28,22 @@
 
 ; return type arrow
 "->" @operator
+
+; types
+(type) @type
+
+; function names
+(function_def (identifier) @function)
+
+(call (identifier) @function.call)
+
+; parameters
+(param (identifier) @variable.parameter)
+
+; variables
+(identifier) @variable
+
+; literals
+(number) @number
+(string) @string
+
