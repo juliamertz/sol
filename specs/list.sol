640d22970ed56d7e35ced337bff9a13c54511f99
Julia Mertz <info@juliamertz.dev>
2025-07-13 23:01:15 +0200

    wip

--- specs/list.sol
+++ specs/list.sol
@@ -1,9 +1,8 @@
 use stdio;
 
 extern func printf(format: Str) -> Int;
-extern func list_push(list: List) -> Int;
 
 func main() -> Int
-  let items : Int[] = [10,10];
-  list_push(items, 20);
+    let items = [];;
+    printf("hello");
 end
