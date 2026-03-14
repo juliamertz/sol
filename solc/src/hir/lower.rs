2a2e4093c3deeb5fc997d1dce31499d7b15bbe84
Julia Mertz <info@juliamertz.dev>
2026-03-14 14:24:07 +0100

    wip: proper recursive lowering

--- solc/src/hir/lower.rs
+++ solc/src/hir/lower.rs
@@ -2,6 +2,7 @@ use miette::Diagnostic;
 use thiserror::Error;
 
 use crate::ast;
+use crate::ext::Boxed;
 use crate::hir::collect::{CollectError, Inventory, collect};
 use crate::hir::{self, HirId};
 use crate::type_checker::{Scope, TypeEnv, TypeError, infer};
@@ -44,17 +45,6 @@ pub fn lower_nodes<'ast>(
     Ok(nodes)
 }
 
-pub fn lower_block<'ast>(
-    block: &'ast ast::Block,
-    env: &mut TypeEnv,
-    scope: &mut Scope<'_>,
-) -> Result<hir::Block<'ast>> {
-    Ok(hir::Block {
-        id: HirId::DUMMY,
-        nodes: lower_nodes(&block.nodes, env, scope)?.into(),
-    })
-}
-
 pub fn lower_module<'ast>(
     module: &'ast ast::Module,
     env: &mut TypeEnv,
@@ -65,29 +55,121 @@ pub fn lower_module<'ast>(
     })
 }
 
+pub fn lower_block<'ast>(
+    block: &'ast ast::Block,
+    env: &mut TypeEnv,
+    scope: &mut Scope<'_>,
+) -> Result<hir::Block<'ast>> {
+    Ok(hir::Block {
+        id: HirId::DUMMY,
+        span: &block.span,
+        nodes: lower_nodes(&block.nodes, env, scope)?.into(),
+    })
+}
+
+pub fn lower_ident<'ast>(
+    ident: &'ast ast::Ident,
+    _env: &mut TypeEnv,
+    _scope: &mut Scope<'_>,
+) -> Result<hir::Ident<'ast>> {
+    Ok(hir::Ident {
+        id: HirId::DUMMY,
+        span: &ident.span,
+        inner: &ident.inner,
+    })
+}
+
 pub fn lower_expr<'ast>(
     expr: &'ast ast::Expr,
     env: &mut TypeEnv,
     scope: &mut Scope<'_>,
 ) -> Result<hir::Expr<'ast>> {
     let kind = match expr {
-        ast::Expr::Ident(ident) => hir::ExprKind::Ident(ident),
-        ast::Expr::Literal(literal) => hir::ExprKind::Literal(literal),
-        ast::Expr::Block(block) => hir::ExprKind::Block(block),
-        ast::Expr::BinOp(bin_op) => hir::ExprKind::BinOp(bin_op),
-        ast::Expr::Prefix(prefix_expr) => hir::ExprKind::Prefix(prefix_expr),
-        ast::Expr::Call(call_expr) => hir::ExprKind::Call(call_expr),
-        ast::Expr::Index(index_expr) => hir::ExprKind::Index(index_expr),
-        ast::Expr::IfElse(if_else) => hir::ExprKind::IfElse(if_else),
-        ast::Expr::List(list) => hir::ExprKind::List(list),
-        ast::Expr::Constructor(constructor) => hir::ExprKind::Constructor(constructor),
-        ast::Expr::MemberAccess(member_access) => hir::ExprKind::MemberAccess(member_access),
+        ast::Expr::Ident(ident) => hir::ExprKind::Ident(lower_ident(ident, env, scope)?),
+        ast::Expr::Literal(literal) => hir::ExprKind::Literal(hir::Literal {
+            id: HirId::DUMMY,
+            span: &literal.span,
+            kind: &literal.kind,
+        }),
+        ast::Expr::Block(block) => hir::ExprKind::Block(lower_block(block, env, scope)?),
+        ast::Expr::BinOp(bin_op) => hir::ExprKind::BinOp(hir::BinOp {
+            id: HirId::DUMMY,
+            span: &bin_op.span,
+            lhs: lower_expr(&bin_op.lhs, env, scope)?.boxed(),
+            op: &bin_op.op,
+            rhs: lower_expr(&bin_op.rhs, env, scope)?.boxed(),
+        }),
+        ast::Expr::Prefix(prefix_expr) => hir::ExprKind::Prefix(hir::Prefix {
+            id: HirId::DUMMY,
+            span: &prefix_expr.span,
+            op: &prefix_expr.op,
+            rhs: lower_expr(&prefix_expr.rhs, env, scope)?.boxed(),
+        }),
+        ast::Expr::Call(call_expr) => hir::ExprKind::Call(hir::Call {
+            id: HirId::DUMMY,
+            span: &call_expr.span,
+            func: lower_expr(&call_expr.func, env, scope)?.boxed(),
+            params: call_expr
+                .params
+                .iter()
+                .map(|param| lower_expr(param, env, scope))
+                .collect::<Result<Vec<_>>>()?
+                .into(),
+        }),
+        ast::Expr::Index(index_expr) => hir::ExprKind::Index(hir::Index {
+            id: HirId::DUMMY,
+            span: &index_expr.span,
+            expr: lower_expr(&index_expr.expr, env, scope)?.boxed(),
+            idx: lower_expr(&index_expr.idx, env, scope)?.boxed(),
+        }),
+        ast::Expr::IfElse(if_else) => hir::ExprKind::IfElse(hir::IfElse {
+            id: HirId::DUMMY,
+            span: &if_else.span,
+            condition: lower_expr(&if_else.condition, env, scope)?.boxed(),
+            consequence: lower_block(&if_else.consequence, env, scope)?,
+            alternative: if_else
+                .alternative
+                .as_ref()
+                .map(|block| lower_block(block, env, scope))
+                .transpose()?,
+        }),
+        ast::Expr::List(list) => hir::ExprKind::List(hir::List {
+            id: HirId::DUMMY,
+            span: &list.span,
+            items: list
+                .items
+                .iter()
+                .map(|expr| lower_expr(expr, env, scope))
+                .collect::<Result<Vec<_>>>()?
+                .into(),
+        }),
+        ast::Expr::Constructor(constructor) => hir::ExprKind::Constructor(hir::Constructor {
+            id: HirId::DUMMY,
+            span: &constructor.span,
+            ident: lower_ident(&constructor.ident, env, scope)?,
+            fields: constructor
+                .fields
+                .iter()
+                .map(|(ident, expr)| {
+                    Ok((
+                        lower_ident(ident, env, scope)?,
+                        lower_expr(expr, env, scope)?,
+                    ))
+                })
+                .collect::<Result<Vec<_>>>()?
+                .into(),
+        }),
+        ast::Expr::MemberAccess(member_access) => hir::ExprKind::MemberAccess(hir::MemberAccess {
+            id: HirId::DUMMY,
+            span: &member_access.span,
+            lhs: lower_expr(&member_access.lhs, env, scope)?.boxed(),
+            ident: lower_ident(&member_access.ident, env, scope)?,
+        }),
         ast::Expr::Ref(expr) => hir::ExprKind::Ref(lower_expr(expr, env, scope)?.into()),
         ast::Expr::RawIdent(_) => todo!(),
     };
     let ty = infer(expr, env, scope)?;
     Ok(hir::Expr {
-        hir_id: HirId::DUMMY,
         kind,
         ty,
         span: expr.span(),
@@ -97,18 +179,59 @@ pub fn lower_expr<'ast>(
 pub fn lower_stmnt<'ast>(
     stmnt: &'ast ast::Stmnt,
     inventory: &mut Inventory<'ast>,
-    _env: &mut TypeEnv,
-    _scope: &mut Scope<'_>,
+    env: &mut TypeEnv,
+    scope: &mut Scope<'_>,
 ) -> Result<Option<hir::Stmnt<'ast>>> {
     Ok(match stmnt {
-        ast::Stmnt::Let(inner) => Some(hir::Stmnt::Let(inner)),
-        ast::Stmnt::Ret(inner) => Some(hir::Stmnt::Ret(inner)),
-        ast::Stmnt::Use(inner) => Some(hir::Stmnt::Use(inner)),
-        ast::Stmnt::Fn(inner) => Some(hir::Stmnt::Fn(inner)),
-        ast::Stmnt::StructDef(def) => Some(hir::Stmnt::StructDef {
-            def,
+        ast::Stmnt::Let(inner) => Some(hir::Stmnt::Let(hir::Let {
+            id: HirId::DUMMY,
+            span: &inner.span,
+            ident: lower_ident(&inner.ident, env, scope)?,
+            ty: inner.ty.as_ref(),
+            val: lower_expr(&inner.val, env, scope)?,
+        })),
+        ast::Stmnt::Ret(inner) => Some(hir::Stmnt::Ret(hir::Ret {
+            id: HirId::DUMMY,
+            span: &inner.span,
+            val: lower_expr(&inner.val, env, scope)?,
+        })),
+        ast::Stmnt::Use(inner) => Some(hir::Stmnt::Use(hir::Use {
+            id: HirId::DUMMY,
+            span: &inner.span,
+            ident: lower_ident(&inner.ident, env, scope)?,
+        })),
+        ast::Stmnt::Fn(func) => Some(hir::Stmnt::Fn(hir::Fn {
+            id: HirId::DUMMY,
+            span: &func.span,
+            is_extern: func.is_extern,
+            ident: lower_ident(&func.ident, env, scope)?,
+            params: func
+                .params
+                .iter()
+                .map(|(ident, ty)| Ok((lower_ident(ident, env, scope)?, ty)))
+                .collect::<Result<Vec<_>>>()?
+                .into(),
+            return_ty: &func.return_ty,
+            body: func
+                .body
+                .as_ref()
+                .map(|body| lower_block(&body, env, scope))
+                .transpose()?,
+        })),
+        ast::Stmnt::StructDef(def) => Some(hir::Stmnt::StructDef(hir::StructDef {
+            id: HirId::DUMMY,
+            span: &def.span,
+            ident: lower_ident(&def.ident, env, scope)?,
+            fields: def
+                .fields
+                .iter()
+                .map(|(ident, ty)| Ok((lower_ident(ident, env, scope)?, ty)))
+                .collect::<Result<Vec<_>>>()?
+                .into(),
             impls: inventory.take_impls(&def.ident).into(),
-        }),
-        _ => None,
+        })),
+
+        // struct impls are handled in the collect phase ^
+        ast::Stmnt::Impl(_) => None,
     })
 }
--- solc/src/hir/mod.rs
+++ solc/src/hir/mod.rs
@@ -1,10 +1,6 @@
-use std::sync::Arc;
-
-use solc_macros::Id;
-
-use crate::ast;
 use crate::lexer::source::Span;
 use crate::type_checker::interner::TypeId;
+use crate::{ast, id};
 
 mod collect;
 mod lower;
@@ -12,49 +8,167 @@ mod lower;
 #[doc(inline)]
 pub use lower::*;
 
-#[derive(Id, Debug, Clone, Copy)]
-pub struct HirId(u32);
+id!(HirId);
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
+pub struct Ident<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub inner: &'ast str,
+}
+
+#[derive(Debug)]
+pub struct Literal<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub kind: &'ast ast::LiteralKind,
+}
+
+#[derive(Debug)]
 pub struct Block<'ast> {
     pub id: HirId,
-    pub nodes: Arc<[Node<'ast>]>,
+    pub span: &'ast Span,
+    pub nodes: Box<[Node<'ast>]>,
+}
+
+#[derive(Debug)]
+pub struct BinOp<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub lhs: Box<Expr<'ast>>,
+    pub op: &'ast ast::Op,
+    pub rhs: Box<Expr<'ast>>,
+}
+
+#[derive(Debug)]
+pub struct Prefix<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub op: &'ast ast::Op,
+    pub rhs: Box<Expr<'ast>>,
+}
+
+#[derive(Debug)]
+pub struct Call<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub func: Box<Expr<'ast>>,
+    pub params: Box<[Expr<'ast>]>,
+}
+
+#[derive(Debug)]
+pub struct Index<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub expr: Box<Expr<'ast>>,
+    pub idx: Box<Expr<'ast>>,
+}
+
+#[derive(Debug)]
+pub struct IfElse<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub condition: Box<Expr<'ast>>,
+    pub consequence: Block<'ast>,
+    pub alternative: Option<Block<'ast>>,
+}
+
+#[derive(Debug)]
+pub struct List<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub items: Box<[Expr<'ast>]>,
+}
+
+#[derive(Debug)]
+pub struct Constructor<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub ident: Ident<'ast>,
+    pub fields: Box<[(Ident<'ast>, Expr<'ast>)]>,
+}
+
+#[derive(Debug)]
+pub struct MemberAccess<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub lhs: Box<Expr<'ast>>,
+    pub ident: Ident<'ast>,
 }
 
 #[derive(Debug)]
 pub enum ExprKind<'ast> {
-    Ident(&'ast ast::Ident),
-    Literal(&'ast ast::Literal),
-    Block(&'ast ast::Block),
-    BinOp(&'ast ast::BinOp),
-    Prefix(&'ast ast::PrefixExpr),
-    Call(&'ast ast::CallExpr),
-    Index(&'ast ast::IndexExpr),
-    IfElse(&'ast ast::IfElse),
-    List(&'ast ast::List),
-    Constructor(&'ast ast::Constructor),
-    MemberAccess(&'ast ast::MemberAccess),
+    Ident(Ident<'ast>),
+    Literal(Literal<'ast>),
+    Block(Block<'ast>),
+    BinOp(BinOp<'ast>),
+    Prefix(Prefix<'ast>),
+    Call(Call<'ast>),
+    Index(Index<'ast>),
+    IfElse(IfElse<'ast>),
+    List(List<'ast>),
+    Constructor(Constructor<'ast>),
+    MemberAccess(MemberAccess<'ast>),
     Ref(Box<Expr<'ast>>),
 }
 
 #[derive(Debug)]
 pub struct Expr<'ast> {
-    pub hir_id: HirId,
     pub kind: ExprKind<'ast>,
     pub ty: TypeId,
     pub span: Span,
 }
 
 #[derive(Debug)]
+pub struct Let<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub ident: Ident<'ast>,
+    pub ty: Option<&'ast ast::Ty>,
+    pub val: Expr<'ast>,
+}
+
+#[derive(Debug)]
+pub struct Ret<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub val: Expr<'ast>,
+}
+
+#[derive(Debug)]
+pub struct Use<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub ident: Ident<'ast>,
+}
+
+#[derive(Debug)]
+pub struct Fn<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub is_extern: bool,
+    pub ident: Ident<'ast>,
+    pub params: Box<[(Ident<'ast>, &'ast ast::Ty)]>,
+    pub return_ty: &'ast ast::Ty,
+    pub body: Option<Block<'ast>>,
+}
+
+#[derive(Debug)]
+pub struct StructDef<'ast> {
+    pub id: HirId,
+    pub span: &'ast Span,
+    pub ident: Ident<'ast>,
+    pub fields: Box<[(Ident<'ast>, &'ast ast::Ty)]>,
+    pub impls: Box<[&'ast ast::Impl]>, // TODO: This should probably also be lowered...
+}
+
+#[derive(Debug)]
 pub enum Stmnt<'ast> {
-    Let(&'ast ast::Let),
-    Ret(&'ast ast::Ret),
-    Use(&'ast ast::Use),
-    Fn(&'ast ast::Fn),
-    StructDef {
-        def: &'ast ast::StructDef,
-        impls: Box<[&'ast ast::Impl]>,
-    },
+    Let(Let<'ast>),
+    Ret(Ret<'ast>),
+    Use(Use<'ast>),
+    Fn(Fn<'ast>),
+    StructDef(StructDef<'ast>),
 }
 
 #[derive(Debug)]
