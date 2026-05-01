f7621867611b4020f7a71d31dce295db467ccfe9
Julia Mertz <info@juliamertz.dev>
2026-05-01 18:27:59 +0200

    feat: fast iterative fibonacci example

--- examples/fibonacci.sol
+++ examples/fibonacci.sol
@@ -9,6 +9,26 @@ func fib(n: i32) -> i32
     end
 end
 
+func fast_fib(n: i32) -> i32
+  if n < 2 then
+    n
+  else
+    let mut curr = 0
+    let mut prev1 = 1
+    let mut prev2 = 0
+
+    let mut i = 2
+    while i < n + 1 do
+      curr = prev1 + prev2
+      prev2 = prev1
+      prev1 = curr
+      i = i + 1
+    end
+
+    curr
+  end
+end
+
 func main() -> i32
     let result = fib(30)
     printf("Result is %d", result)
